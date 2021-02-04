eb4662e9e0ad7697e86f9d355ddf2a33a6afd6be
Sarah Fitzsimons <sarahfitz2713@gmail.com>
2021-02-04 15:52:05 -0700

    Add work on meal and meal-test

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,31 @@
-function nameMenuItem() {
-
+function nameMenuItem(name) {
+  return `Delicious ${name}`
 }
+
+function createMenuItem(name, price, type) {
+  return {
+    name:name,
+    price:price,
+    type:type,
+  }
+}
+
+function addIngredients(ingredientName, ingredientsArray) {
+  if (ingredientsArray.indexOf(ingredientName) === -1)
+  return ingredientsArray.push(ingredientName);
+}
+
+function formatPrice(initialPrice) {
+  
+}
+
 
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
+  createMenuItem,
+  addIngredients,
+  formatPrice,
   // decreasePrice,
   // createRecipe
 }
-
-
