93faa18bcba37b9590fb7d49186922ba5fd88e1c
Sarah Fitzsimons <sarahfitz2713@gmail.com>
2021-02-06 16:10:16 -0700

    Add answers for first half of restaurant.js

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,26 @@
+function createRestaurant(nameString) {
+  return {
+    name: nameString,
+    menus: {
+      breakfast: [],
+      lunch: [],
+      dinner: [],
+    }
+  };
+}
+
+function addMenuItem(restaurantObject, menuObject) {
+  restaurantObject.menus.lunch.push(menuObject);
+  //console.log(restaurantObject);
+  //console.log(menuObject);
+  //console.log(restaurantObject.menus.lunch);
+}
+
+
 
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
+  createRestaurant,
+  addMenuItem,
   // removeMenuItem
 }
