7e0f798016e4ccab12dbe9a2257a9265c450ecc5
Sarah Fitzsimons <sarahfitz2713@gmail.com>
2021-02-05 18:57:34 -0700

    Add first function written for order.js

--- src/order.js
+++ src/order.js
@@ -1,7 +1,27 @@
+function takeOrder(newOrder, deliveryOrders) {
+  if (deliveryOrders.length < 3) {
+  return deliveryOrders.push(newOrder);
+  }
+}
+
+function refundOrder(num1, deliveryOrders) {
+  return deliveryOrders.splice(num1);
+}
+
+
+
+
+
+
+
+
+
+
+
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
+   takeOrder,
+  refundOrder,
   // listItems,
   // searchOrder
 }
